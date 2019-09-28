8ad01d8a0c50b7f89a9ebe0426fd4dcd503b13ff
David Marro <davidmarro@mbp-di-david.homenet.telecomitalia.it>
2019-09-28 19:51:01 +0200

    inserimento segue e settaggio

--- WhatsApp/WhatsApp/ViewControllers/MainController.swift
+++ WhatsApp/WhatsApp/ViewControllers/MainController.swift
@@ -10,21 +10,32 @@ import UIKit
 
 class MainController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var buttonLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        buttonLogin.layer.cornerRadius = 27
     }
     
 
-    /*
-    // MARK: - Navigation
+       // MARK: - Actions
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func goToLogin(_ sender: Any) {
+        performSegue( withIdentifier: "goToLogin", sender: self)
+        
+       
     }
-    */
+    
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+  {
+      if segue.identifier == "fakeButtonPressed"
+      {
+       
+      }
+  }
 
+    
+    @IBAction func goToSignUp(_ sender: Any) {
+    }
 }
