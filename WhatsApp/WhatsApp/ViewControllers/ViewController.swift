a114fffb780c00e01f062dc84e70d308d6fbf665
David Marro <davidmarro@mbp-di-david.homenet.telecomitalia.it>
2019-09-28 18:15:51 +0200

    inserimento immagine nel main e impostazione del primo view controller

---
+++ WhatsApp/WhatsApp/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsApp
+//
+//  Created by David Marro on 26/09/2019.
+//  Copyright © 2019 David Marro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
